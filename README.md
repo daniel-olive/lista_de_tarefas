22e67378b1f2a08f066f150a9dbd6cea20124b16
Daniel Oliveira <daniel.olive.dev@gmail.com>
2023-09-27 23:54:51 -0300

    Lista de Tarefas

---
+++ README.md
@@ -0,0 +1,16 @@
+# Projeto Lista de Tarefas
+
+
+## Tecnologias usadas.
+
+- ReactJS
+- TypeScript
+- TailwindCSS
+
+
+## Método aplicado.
+
+- Reducers => são funções que implementam o comportamento das ações.
+
+## O que é o Reducer?
+- Reducers são funções puras (funções que não geram efeitos colaterais, isto é, para a mesma entrada, temos a mesma saída) com a capacidade de disparar eventos e que podem alterar um atributo da store, evoluindo o estado global da aplicação.
---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useState, useReducer } from "react";
+import { listReducer } from "./reducers/listReducer";
+import "./App.css";
+
+const App = () => {
+	const [list, dispatch] = useReducer(listReducer, []);
+	const [addField, setAddField] = useState("");
+
+	const handleAddButton = () => {
+		if (addField.trim() === "") return false;
+
+		dispatch({
+			type: "add",
+			payload: { text: addField.trim() },
+		});
+		setAddField("");
+	};
+
+	const handleDoneCheckbox = (id: number) => {
+		dispatch({
+			type: "toggleDone",
+			payload: { id },
+		});
+	};
+
+	const handleEdit = (id: number) => {
+		const item = list.find((it) => it.id === id);
+		if (!item) return false;
+
+		const newText = window.prompt("Editar Tarefa", item.text);
+		if (!newText || newText?.trim() === "") return false;
+
+		dispatch({
+			type: "editText",
+			payload: { id, newText },
+		});
+	};
+	const handleExcluir = (id: number) => {
+		if (!window.confirm("Tem certeza que deseja excluir?")) return false;
+		dispatch({
+			type: "remove",
+			payload: { id },
+		});
+	};
+
+	return (
+		<div className="container mx-auto">
+			<h1 className="text-center text-4xl my-4">Lista de Tarefas</h1>
+			<div className="max-w-2xl mx-auto flex rounded-md bg-gray-900 border border-gray-400 p-4 my-4">
+				<input className="flex-1 rounded-md border border-white p-3 bg-transparent text-white outline-none" type="text" placeholder="Digite um Item" value={addField} onChange={(e) => setAddField(e.target.value)} />
+				<button className="hover:bg-gray-700 rounded-md ml-2 p-4" onClick={handleAddButton}>
+					Adicionar
+				</button>
+			</div>
+			<ul className="max-w-2xl mx-auto">
+				{list.map((item) => (
+					<li key={item.id} className="flex items-center p-3 my-3 border-b border-gray-700">
+						<input onClick={() => handleDoneCheckbox(item.id)} type="checkbox" className="w-4 h-4 mr-4" />
+						<p className="flex-1 text-lg">{item.text}</p>
+						<button onClick={() => handleEdit(item.id)} className="mx-4 text-white hover:text-gray-500">
+							Editar
+						</button>
+						<button onClick={() => handleExcluir(item.id)} className="mx-4 text-white hover:text-gray-500">
+							Excluir
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default App;
